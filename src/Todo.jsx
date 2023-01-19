e9c9674a77eb633ec8e98fc84c7bd9d5d2b3423c
Swapnoneel Saha <121167506+Swpn0neel@users.noreply.github.com>
2023-01-19 19:23:03 +0530

    Update Todo.jsx

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -7,7 +7,7 @@ const Todo = () => {
   const [todoDetails, setTodoDetails] = useState();
 
   useEffect(() => {
-    fetch(`https://my-todo.free.beeceptor.com/todo/${id}`)
+    fetch(`https://my-todo.proxy.beeceptor.com/todo/${id}`)
       .then((response) => {
         if (response.ok) return response.json();
         else throw new Error(response.status);
